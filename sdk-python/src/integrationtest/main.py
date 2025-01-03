d3a188049e78a764bb25263c9380eca7295311a4
cploujoux <ch.ploujoux@gmail.com>
2025-01-03 18:00:50 +0100

    Update test for python

--- sdk-python/src/integrationtest/main.py
+++ sdk-python/src/integrationtest/main.py
@@ -1,18 +1,31 @@
-from beamlit.api.models import get_model_deployment
+from beamlit.api.models import get_model, list_models
 from beamlit.authentication import get_authentication_headers, new_client
 from beamlit.common.settings import init
-from beamlit.models.model_deployment import ModelDeployment
+from beamlit.models import Model
 from beamlit.run import RunClient
 
 settings = init()
 client = new_client()
-model_deployment: ModelDeployment = get_model_deployment.sync(
-    "gpt-4o-mini", "production", client=client
+models = list_models.sync(client=client)
+model: Model = get_model.sync(
+    "gpt-3-5-turbo", client=client
 )
 # init_agent(client=client)
 run_client = RunClient(client=client)
 response = run_client.run(
-    "function", "math", settings.environment, method="POST", json={"query": "4+4"}
+    "model",
+    "gpt-3-5-turbo",
+    settings.environment,
+    method="POST",
+    path="/v1/chat/completions",
+    json={
+        "messages": [
+            {
+            "role": "user",
+            "content": "Hello!"
+            }
+        ]
+    }
 )
 
 if response.status_code == 200:
