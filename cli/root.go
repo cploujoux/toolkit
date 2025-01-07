932474aabe728f809829be724444c75d20987817
cploujoux <ch.ploujoux@gmail.com>
2025-01-07 09:45:53 +0100

    Update registry url

--- cli/root.go
+++ cli/root.go
@@ -11,14 +11,14 @@ import (
 var BASE_URL = "https://api.beamlit.com/v0"
 var APP_URL = "https://app.beamlit.com"
 var RUN_URL = "https://run.beamlit.com"
-var REGISTRY_URL = "https://serverless-registry-production.beamlit.workers.com"
+var REGISTRY_URL = "https://us.registry.beamlit.com"
 
 func init() {
 	if os.Getenv("BL_ENV") == "dev" {
 		BASE_URL = "https://api.beamlit.dev/v0"
 		APP_URL = "https://app.beamlit.dev"
 		RUN_URL = "https://run.beamlit.dev"
-		REGISTRY_URL = "https://serverless-registry-production.beamlit.workers.dev"
+		REGISTRY_URL = "https://eu.registry.beamlit.dev"
 	}
 }
 
