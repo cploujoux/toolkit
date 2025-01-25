98ad9e6449474a098709d7562f1266a893c64644
cploujoux <ch.ploujoux@gmail.com>
2025-01-25 08:47:31 -0800

    Add import deploy export

--- sdk-ts/src/index.ts
+++ sdk-ts/src/index.ts
@@ -7,3 +7,4 @@ export * from "./common/index.js";
 export * from "./client/index.js";
 export * from "./authentication/index.js";
 export * from "./agents/index.js";
+export * from "./deploy/deploy.js";
