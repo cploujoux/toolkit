05cd503469d67a6ad8ed8f8580ea4ad03145c349
cploujoux <ch.ploujoux@gmail.com>
2025-02-13 15:12:04 -0800

    Update qdrant error handler

--- sdk-ts/src/knowledgebase/qdrant.ts
+++ sdk-ts/src/knowledgebase/qdrant.ts
@@ -43,22 +43,84 @@ export class QdrantKnowledgebase implements KnowledgebaseClass {
     });
   }
 
+  handleError(action: string, error: Error) {
+    if (error instanceof Error && "status" in error) {
+      if (
+        "data" in error &&
+        typeof error.data === "object" &&
+        error.data &&
+        "status" in error.data &&
+        typeof error.data.status === "object" &&
+        error.data.status &&
+        "error" in error.data.status
+      ) {
+        return new Error(
+          `Qdrant http error for ${action}: ${error.status} - ${error.data.status.error}`
+        );
+      } else {
+        return new Error(
+          `Qdrant http error for ${action}: ${error.status} - ${error.message}`
+        );
+      }
+    }
+    return error;
+  }
+
+  async getOrCreateCollection(embeddings: {
+    size: number;
+    distance: string;
+  }): Promise<void> {
+    try {
+      const response = await this.client.getCollections();
+      if (
+        !response.collections.find(
+          (collection: any) => collection.name === this.config.collectionName
+        )
+      ) {
+        await this.client.createCollection(this.config.collectionName, {
+          vectors: {
+            default: {
+              size: embeddings.size,
+              distance: embeddings.distance,
+            },
+          },
+        });
+      }
+    } catch (error: any) {
+      if (
+        error instanceof Error &&
+        error.message.includes("Error creating collection ApiError: Conflict")
+      ) {
+        return this.getOrCreateCollection(embeddings);
+      }
+      throw this.handleError("creating collection", error as Error);
+    }
+  }
+
   async add(key: string, value: string, infos?: any): Promise<void> {
-    const embedding = await this.embeddingModel.embed(value);
-    await this.client.upsert(this.collectionName, {
-      points: [
-        {
-          id: key,
-          vector: {
-            default: embedding,
+    try {
+      const embedding = await this.embeddingModel.embed(value);
+      await this.getOrCreateCollection({
+        size: embedding.length,
+        distance: infos?.distance || "Cosine",
+      });
+      await this.client.upsert(this.collectionName, {
+        points: [
+          {
+            id: key,
+            vector: {
+              default: embedding,
+            },
+            payload: {
+              text: value,
+              ...infos,
+            },
           },
-          payload: {
-            text: value,
-            ...infos,
-          },
-        },
-      ],
-    });
+        ],
+      });
+    } catch (error) {
+      throw this.handleError("adding", error as Error);
+    }
   }
 
   async search(
@@ -67,26 +129,34 @@ export class QdrantKnowledgebase implements KnowledgebaseClass {
     scoreThreshold?: number,
     limit?: number
   ): Promise<Array<KnowledgebaseSearchResult>> {
-    const embedding = await this.embeddingModel.embed(query);
-    const results = await this.client.query(this.collectionName, {
-      query: embedding,
-      using: "default",
-      with_payload: true,
-      score_threshold: scoreThreshold || this.scoreThreshold,
-      limit: limit || this.limit,
-    });
-    return results.points.map((point: any) => {
-      return {
-        key: point.id,
-        value: JSON.stringify(point.payload),
-        similarity: point.score,
-      };
-    });
+    try {
+      const embedding = await this.embeddingModel.embed(query);
+      const results = await this.client.query(this.collectionName, {
+        query: embedding,
+        using: "default",
+        with_payload: true,
+        score_threshold: scoreThreshold || this.scoreThreshold,
+        limit: limit || this.limit,
+      });
+      return results.points.map((point: any) => {
+        return {
+          key: point.id,
+          value: JSON.stringify(point.payload),
+          similarity: point.score,
+        };
+      });
+    } catch (error) {
+      throw this.handleError("searching", error as Error);
+    }
   }
 
   async delete(key: string): Promise<void> {
-    await this.client.delete(this.collectionName, {
-      points: [key],
-    });
+    try {
+      await this.client.delete(this.collectionName, {
+        points: [key],
+      });
+    } catch (error) {
+      throw this.handleError("deleting", error as Error);
+    }
   }
 }
